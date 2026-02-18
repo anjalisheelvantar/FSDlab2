2132e7ed329cb5d100f548354ec53fadab328bea
anjalisheelvantar <anjalisheelvantar@gmail.com>
2026-02-18 21:09:36 +0530

    FSD lab2

---
+++ app/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './components/navbar'
+import Home from './components/home'
+import About from './components/about'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+      <div>
+        <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+         </BrowserRouter>
+      </div>
+  )
+}
+export default App;
---
+++ app/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}> 
+    <h1>Welcome to the Home Page</h1>
+    <p>This component was created using a standard Function.</p> </div>
+  )
+}
